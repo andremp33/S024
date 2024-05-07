0c92350e6d420133f41dcea023d71c3f44a3209f
andrempinto <115936934+A104267@users.noreply.github.com>
2024-05-07 20:25:10 +0100

    Update status.h

--- SO/include/status.h
+++ SO/include/status.h
@@ -1,14 +1,15 @@
 #ifndef STATUS_H
 #define STATUS_H
 
+#include "../include/task.h"
 
 char* status_schedule_tasks();
 char* status_executed_tasks();
 char* status_executing_tasks();
 char* status_server_state();
-void status_init();
-void status_add_task();
-void status_remove_task();
+void status_init(int max_parallel_tasks);
+void status_add_task(Task task_executing);
+void status_remove_task(Task task_executed);
 
 
 
